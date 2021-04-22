38f626183494179576ed94b2433e13141e05da69
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-04-22 16:09:04 -0500

    Updates

---
+++ precache-manifest.b86ae9287734e7cc7c62e98692056256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "205515d0011b04bf50d3",
+    "url": "/bc_website/static/css/main.6da85a15.chunk.css"
+  },
+  {
+    "revision": "205515d0011b04bf50d3",
+    "url": "/bc_website/static/js/main.205515d0.chunk.js"
+  },
+  {
+    "revision": "d7a437bedc99c7fd2d97",
+    "url": "/bc_website/static/css/1.07282c18.chunk.css"
+  },
+  {
+    "revision": "d7a437bedc99c7fd2d97",
+    "url": "/bc_website/static/js/1.d7a437be.chunk.js"
+  },
+  {
+    "revision": "ca9034b47e9e72b44518",
+    "url": "/bc_website/static/js/runtime~main.ca9034b4.js"
+  },
+  {
+    "revision": "9867ed23a1b8655a4b60762eae98f81f",
+    "url": "/bc_website/index.html"
+  }
+];
